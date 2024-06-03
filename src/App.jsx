f0aeb0ba34c79607f9ac9a4331d7194d3c13b987
Miran <222miran222@gmail.com>
2024-06-04 04:04:57 +0530

    not found

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,55 @@
-import React, { useState } from "react";
+import React from "react";
 import Header from "./components/Header/Header";
 import "@radix-ui/themes/styles.css";
 import Home from "./pages/Home";
 import SideBar from "./components/SideBar/SideBar";
 import Footer from "./components/Footer/Footer";
 import Popup from "./components/Popup/Popup";
-import Authentication from "./components/Authentication/Authentication";
-import AuthPopup from "./components/Authentication/AuthPopup";
 import SignUpForm from "./pages/SignUpForm";
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from "react-router-dom";
 import YourProfile from "./pages/YourProfile";
+import NotFound from "./pages/NotFound";
+
 function App() {
+  const location = useLocation();
+
+  const knownRoutes = ["/", "/signup", "/YourProfile"];
+  const isNotFoundPage = !knownRoutes.includes(location.pathname);
+
   return (
     <div className="font-sans">
-      <div className="">
-        <Popup />
-      </div>
+      {!isNotFoundPage && (
+        <>
+          <div className="">
+            <Popup />
+          </div>
+          <nav className="bg-[#83c5be]">
+            <Header />
+            <SideBar />
+          </nav>
+        </>
+      )}
 
-      <nav className="bg-[#83c5be]">
-        <Header />
-        <SideBar />
-      </nav>
-
-      <main className="pt-16 w-full pl-0 md:pt-16 lg:pl-72 bg-[#edf6f9]">
-      <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/signup" element={<SignUpForm />} />
-      <Route path="/YourProfile" element={<YourProfile />} />
-      </Routes>
+      <main
+        className={`pt-16 w-full ${
+          !isNotFoundPage ? "pl-0 md:pt-16 lg:pl-72" : ""
+        } bg-[#edf6f9]`}
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUpForm />} />
+          <Route path="/YourProfile" element={<YourProfile />} />
+          <Route path="*" element={<NotFound />} /> {/* 404 page route */}
+        </Routes>
       </main>
 
-
-      <footer className="lg:pl-72 pl-0">
-        <Footer />
-      </footer>
+      {!isNotFoundPage && (
+        <footer className="lg:pl-72 pl-0">
+          <Footer />
+        </footer>
+      )}
     </div>
   );
 }
 
-
 export default App;
-
-
-
-      
-    
